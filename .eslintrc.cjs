92f60fef92edf91e95a605f5e3116c2c3fb35bf5
OmarMAttia7 <omarmattia97@gmail.com>
2023-09-27 19:17:08 +0300

    refactor: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,25 @@
 /* eslint-env node */
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.test.json', 'tsconfig.build.json'],
-    tsconfigRootDir: __dirname
+    project: ["tsconfig.json", "tsconfig.test.json", "tsconfig.build.json"],
+    tsconfigRootDir: __dirname,
   },
   rules: {
-    '@typescript-eslint/consistent-type-definitions': 'error',
-    '@typescript-eslint/consistent-type-exports': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }]
+    "@typescript-eslint/consistent-type-definitions": "error",
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+    ],
   },
-  plugins: ['@typescript-eslint'],
-  root: true
+  plugins: ["@typescript-eslint"],
+  root: true,
 };
