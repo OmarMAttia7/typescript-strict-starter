9e186934fe27bc47d17560cdac9c58d9a1655568
OmarMAttia7 <omarmattia97@gmail.com>
2023-10-13 09:29:33 +0300

    feat: better test file structure

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "node:assert";
 import { test } from "node:test";
-import { msg } from "@src/main.js";
+import { msg } from "./main.js";
 
 await test("Hello, world!", () => {
   assert.strictEqual(msg, "Hello, world!");
