308350ae22124dc779c0aa3c947c334f8264319c
OmarMAttia7 <omarmattia97@gmail.com>
2023-12-23 00:02:01 +0200

    refactor: better test runner error handling

--- src/test.ts
+++ src/test.ts
@@ -5,21 +5,31 @@ import { run } from 'node:test';
 import { spec } from 'node:test/reporters';
 
 async function main() {
-	await setup();
-	const files = await fetchTestFilePaths();
-	runTests({
-		files,
-	})(teardown);
+	try {
+		await setup();
+		const files = await fetchTestFilePaths();
+		runTests({
+			files,
+		})(teardown);
+	} catch (err) {
+		await teardown();
+		throw err;
+	}
 }
 
 main();
 
 async function fetchTestFilePaths() {
 	const basePath = resolve('./build');
-	const dirFiles = await readdir(basePath, { recursive: true });
-	return dirFiles
-		.filter((file) => file.endsWith('.test.js'))
-		.map((file) => resolve(basePath, file));
+	const dirFiles = await readdir(basePath, { recursive: true }).then((files) =>
+		files
+			.filter((file) => file.endsWith('.test.js'))
+			.map((file) => resolve(basePath, file)),
+	);
+	if (dirFiles.length === 0) {
+		throw new Error('No test files found');
+	}
+	return dirFiles;
 }
 
 async function setup() {
