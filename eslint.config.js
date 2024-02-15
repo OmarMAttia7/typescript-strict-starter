ebb312aa2ae9325fa5ec3fa6cd4003de82023be8
OmarMAttia7 <omarmattia97@gmail.com>
2024-02-15 19:36:30 +0200

    feat: finish formatting and linting with eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,12 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPrettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+  eslintPrettierConfig,
   {
     languageOptions: {
       parserOptions: {
